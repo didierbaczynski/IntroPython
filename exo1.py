b8fd1592ba98d392e50445ef072ab60dcd89e7d0
Didier Baczynski <didier.baczynski@gmail.com>
2019-11-10 10:27:30 -0500

    Test VS

---
+++ exo1.py
@@ -0,0 +1,9 @@
+L1 = ['el1.1','el1.2','el1.3','el1.4']
+L2 = ['el2.1','el2.2','el2.3','el2.4']
+L3 =[]
+for index,element in enumerate(L1):
+    if index%2 == 0 :
+      L3.append(L1[index])
+    else :
+      L3.append(L2[index])
+print (L3)
