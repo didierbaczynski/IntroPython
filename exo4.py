decfe3c38e7be31278d668cdba778bcd651d727d
Didier Baczynski <didier.baczynski@gmail.com>
2019-11-10 21:18:29 -0500

    exo4

---
+++ exo4.py
@@ -0,0 +1,15 @@
+L1=['e1','e2','e3','e4','e1','e5','e3','e5','e8','e2']
+searchedList=[]
+
+count = 0
+for index,element in enumerate(L1) :
+    elementToSearch = element
+    if not(elementToSearch in searchedList) :
+        searchedList.append (elementToSearch)
+        count = 1
+        for i in range(index+1,len(L1)):
+            if elementToSearch == L1[i]:
+                count=count+1
+        print (elementToSearch,' is ',count,' times in the list')
+    count = 0   
+
