cc23eb5d812dc32e537ec4578d8cb2a923963738
Didier Baczynski <didier.baczynski@gmail.com>
2019-11-11 10:52:27 -0500

    Exo7 with for loop

--- exo7.py
+++ exo7.py
@@ -1,7 +1,8 @@
 # Program to display the Fibonacci sequence up to n-th term
-nterms = int(input("How many terms? "))
+nterms = 100
 # first two terms
-n1, n2 = 0, 1
+n1 = 0
+n2 = 1
 count = 0
 # check if the number of terms is valid
 if nterms <= 0:
@@ -11,10 +12,9 @@ elif nterms == 1:
    print(n1)
 else:
    print("Fibonacci sequence:")
-   while count < nterms:
+   for i in range(0,nterms):
        print(n1)
        nth = n1 + n2
        # update values
        n1 = n2
        n2 = nth
-       count += 1
